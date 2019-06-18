88b95daf799a7328613239eb529d44599db9cc62
chellamuthu <51902564+chellamuthu@users.noreply.github.com>
2019-06-18 14:49:51 +0530

    Update bag15.py

--- bag15.py
+++ bag15.py
@@ -1,5 +1,5 @@
-year=int(input("Enter year to be checked:"))
-if(year%4==0 and year%100!=0 or year%400==0):
+years=int(input("Enter year to be checked:"))
+if(years%4==0 and years%100!=0 or years%400==0):
     print("The year is a leap year!)
 else:
     print("The year isn't a leap year!)
